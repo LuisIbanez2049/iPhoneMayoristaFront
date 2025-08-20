c6292a061f30d2d52693a65165a1df834ff6d8e6
unknown <luiszyzz248@gmail.com>
2025-08-20 00:54:42 -0300

    Agregue las terjetas de categorias y las tarjetas de productos y aplique la opcion de filtrar por categoria de forma dinamica con los datos del backEnd

---
+++ src/components/CardProduct.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function CardProduct({img, name, price}) {
+  return (
+    <div className='w-[260px] flex flex-col items-center py-[8px] gap-4 rounded-xl shadow-xl hover:scale-[101%] hover:shadow-xl transition-all duration-500' 
+    style={{boxShadow: "0px 2px 8px rgba(0, 0, 0, 0.1)",}}>
+        <div className='w-[240px] h-[310px]' style={{
+      backgroundImage: `url(${img})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat',
+    }}>
+
+        </div>
+        <h1 className='w-[240px] font-semibold '>{name}</h1>
+        <h1 className='w-[240px]'>$49.00</h1>
+    </div>
+  )
+}
+
+export default CardProduct
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,10 +1,28 @@
 import React, { useEffect, useState } from 'react'
 import CardFilter from '../components/CardFilter'
 import axios from 'axios'
+import CardProduct from '../components/CardProduct'
 
 function Products() {
 
   const [products, setProducts] = useState([])
+  const [filteredProducts, setFilteredProducts] = useState([])
+  const [categoryId, setCategoryId] = useState(0)
+  const[nameFilteredCategory, setNameFilteredCategory] = useState("")
+  const [categorias, setCategorias] = useState([])
+  const [seeFilteredProduct, setSeeFilteredProduct] = useState(false)
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/category/")
+    .then((response) => {
+      console.log(response.data)
+      setCategorias(response.data)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+
+  }, [])
 
   useEffect(() => {
     axios.get("http://localhost:8080/api/product/")
@@ -17,18 +35,54 @@ function Products() {
     })
 
   }, [])
+
+  const actualizarCategoryId = (id, name) =>{
+    console.log("Mi id es: " + id)
+    setCategoryId(id)
+    setNameFilteredCategory(name)
+    filtrarProductos(id)
+    setSeeFilteredProduct(true)
+  }
+
+  function filtrarProductos(categoriaId){
+    const productosFiltrados = products.filter(product => product.categoryId == categoriaId)
+    setFilteredProducts(productosFiltrados)
+  }
+
   return (
-    <div>
+    <div className='my-[50px] px-[5%] pb-[2%] border border-black'>
         Products
         <div className='w-[100%] flex flex-row justify-center gap-8 flex-wrap'>
-          {products && products.length > 0 && products.map((product) => {
+          {categorias && categorias.length > 0 && categorias.map((categoria) => {
             return (
-              <CardFilter/>
+              <CardFilter id={categoria.id} img={categoria.img} name={categoria.name} onActualizarId={actualizarCategoryId}/>
             )
           })}
             
-            {/* <CardFilter/>
-            <CardFilter/> */}
+        </div>
+
+        <div className={`w-[100%] flex flex-row justify-end mt-[30px] ${seeFilteredProduct ? "show" : "hidden"}`}>
+          <button className='text-[15px] font-semibold text-[#00000096]' onClick={() => setSeeFilteredProduct(false)}> VER TODOS LOS PRODUCTOS</button>
+        </div>
+
+        <div className={`w-[100%] flex flex-row justify-center px-[10%] mt-[80px] gap-8 flex-wrap ${seeFilteredProduct ? "hidden" : "show"}`}>
+          {products && products.length > 0 && products.map((product) => {
+            return (
+              <CardProduct img={product.firstImage} name={product.name}/>
+            )
+          })}
+            
+        </div>
+
+
+        <h1 className={`font-bold text-[35px] mt-[50px] text-[#000000c5] ${seeFilteredProduct ? "show" : "hidden"}`}>{nameFilteredCategory}</h1>
+        <div className={`w-[100%] flex flex-row justify-center px-[10%] gap-8 flex-wrap ${seeFilteredProduct ? "show" : "hidden"}`}>
+          {filteredProducts && filteredProducts.length > 0 && filteredProducts.map((product) => {
+            return (
+              <CardProduct img={product.firstImage} name={product.name}/>
+            )
+          })}
+            
         </div>
 
         
